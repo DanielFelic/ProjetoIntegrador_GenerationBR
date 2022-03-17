b0998c113c1406e376ea5aa1f56a519394aaa9a3
Daniel Feliciano <dani.felic7@gmail.com>
2022-03-17 17:13:05 -0300

    Tela de publicação

---
+++ post_screen_don/app/src/main/java/com/generation/post_screen_don/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.generation.post_screen_don
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
